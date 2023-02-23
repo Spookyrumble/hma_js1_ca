680a81c89eca57268560e1d74d67ff46eb35adc2
HMAsp <hanand55935@stud.noroff.no>
2023-02-23 10:09:09 +0100

    Update details.js
    ingredient load adjustment

--- js/details.js
+++ js/details.js
@@ -30,8 +30,8 @@ async function fetchBeer() {
                                     </div>
                                     <div>
                                         <h3 class="details_h2">Main ingredients:</h3>
-                                        <p class="details_p">Hops: ${details.ingredients.hops[0].name} and ${details.ingredients.hops[1].name}</p>
-                                        <p class="details_p">Malt: ${details.ingredients.malt[0].name} and ${details.ingredients.malt[1].name}</p>
+                                        <p class="details_p">Hops: ${details.ingredients.hops[0].name}</p>
+                                        <p class="details_p">Malt: ${details.ingredients.malt[0].name}</p>
                                     </div>
                                     <div>
                                         <h3 class="details_h2">This beer goes well with:</h3>
